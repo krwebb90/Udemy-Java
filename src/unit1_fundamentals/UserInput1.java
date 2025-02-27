a6b3c13adda8d627d73f2529d5f84bfdf7292b2a
krwebb90 <krwebb90@gmail.com>
2025-02-27 15:57:30 -0600

    End of Section 3

--- src/unit1_fundamentals/UserInput1.java
+++ src/unit1_fundamentals/UserInput1.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class UserInput1
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the name of the user");
         String name = sc.nextLine();
