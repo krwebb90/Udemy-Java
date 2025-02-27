6375a83d143cd98766f5cf532013692b5d9658db
krwebb90 <krwebb90@gmail.com>
2025-02-27 15:22:33 -0600

    End of Section 3

--- src/unit1_fundamentals/HelloWorld.java
+++ src/unit1_fundamentals/HelloWorld.java
@@ -1,6 +1,8 @@
+package unit1_fundamentals;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main
+public class HelloWorld
 {
     public static void main(String[] args)
     {
